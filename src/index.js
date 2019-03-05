f686df5b144cdfb883f6a175bee39b176ed1c266
lovelyladylove <lovewagemann@gmail.com>
2019-03-05 15:22:49 +0100

    router

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+
 import './index.css';
 import AuthorQuiz from './AuthorQuiz';
 import * as serviceWorker from './serviceWorker';
@@ -76,8 +78,25 @@ function onAnswerSelected(answer) {
     render();
 }
 
+function AddAuthorForm({match}) {
+    return <div>
+        <h1>Add Author</h1>
+        <p>{JSON.stringify(match)}</p>
+    </div>
+}
+
+function App() {
+    return <AuthorQuiz {...state} onAnswerSelected={onAnswerSelected} />;
+}
+
 function render() {
-    ReactDOM.render(<AuthorQuiz {...state} onAnswerSelected={onAnswerSelected} />, document.getElementById('root'));
+    ReactDOM.render(
+    <BrowserRouter>
+        <React.Fragment>
+            <Route exact path="/" component={App} />
+            <Route path="/add" component={AddAuthorForm} />
+        </React.Fragment>
+    </BrowserRouter>, document.getElementById('root'));
 }
 render();
 serviceWorker.unregister();
