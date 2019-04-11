520c59a3c756d14644e8140ce74a03a1f26fad1f
lovelyladylove <lovewagemann@gmail.com>
2019-04-11 13:26:31 +0200

    Updates

---
+++ precache-manifest.2fad3db8b5128245934ecf70fbb71c78.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba0040e62579613d99c0",
+    "url": "/AuthorQuiz/static/js/runtime~main.500bd57c.js"
+  },
+  {
+    "revision": "191ecea470a4b9df3e4d",
+    "url": "/AuthorQuiz/static/js/main.fcb638e3.chunk.js"
+  },
+  {
+    "revision": "26722b92a59c9d741f09",
+    "url": "/AuthorQuiz/static/js/2.1cd514eb.chunk.js"
+  },
+  {
+    "revision": "191ecea470a4b9df3e4d",
+    "url": "/AuthorQuiz/static/css/main.ea82a2b0.chunk.css"
+  },
+  {
+    "revision": "af6dcb3cdea84d2a064b5ef26241b0f5",
+    "url": "/AuthorQuiz/index.html"
+  }
+];
